cb2ff2ceb9d6dee8b76229af7abb89f9f8d6ddc7
Cesar Aguilar <Blueavian9@gmail.com>
2024-10-13 17:01:09 -0700

    updated Form.js

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -1,52 +1,53 @@
-import React from 'react';
+import React from "react";
 
-const  Form = {props} => {
-    const { change, submit } = props;
-    const { username, email, password, tos } = props.values;
+const Form = (props) => {
+  const { change, submit } = props;
+  const { username, password, tos } = props.values;
 
-    const onSubmit = (e) => {
-        const { name, value, checked, type } e.target();
-        const newVal = type === "checkbox" ? checked : value;
-        change{name, newVal};
-    }
+  const onChange = (e) => {
+    const { name, value, checked, type } = e.target;
+    const newVal = type === "checkbox" ? checked : value;
+    change({ name, newVal });
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        submit();
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    submit();
+  };
 
-    return {
-       <div>
-           <h1> My cool form! </h1> 
-           <form>
-            <label>Name:
-                <input
-                    type="text"
-                    name="username"
-                    value="username"
-                    onChange={onChange}
-                />
-            </label>
-            <label>Password
-                <input 
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={onChange}
-                />
-            </label>
-            <label>Terms of Service: 
-                <input 
-                    type="checkbox"
-                    name="tos"
-                    checked={tos}
-                    onChange={onChange}
-                /> 
-            </label>
-            <input type="submit" value="Create a Friend!" />
-          </form>
-        </div> 
-    )
-} 
+  return (
+    <div>
+      <h1>My Cool Form!</h1>
+      <form onSubmit={onSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={onChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </label>
+        <br />
+        <label>
+          Terms of Service:
+          <input type="checkbox" name="tos" checked={tos} onChange={onChange} />
+        </label>
+        <br />
+        <input type="submit" value="Create a Friend!" />
+      </form>
+    </div>
+  );
+};
 
-export default form;
+export default Form;
