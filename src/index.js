146c083e1b2884bdfb1f514fc13279b855a8a98d
Cesar Aguilar <Blueavian9@gmail.com>
2024-10-17 13:38:27 -0700

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
-import Form from "./Form.css"; // Correct way to import CSS
-// Remove the incorrect import
+import "./index.css"; // Keep this if you have global styles
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -12,7 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
